945e5d2b80acee0ed36d42899716156cc22923ff
SPG-ADR13N <163685444+SPG-ADR13N@users.noreply.github.com>
2026-06-08 15:00:32 +0200

    Update command_toggles.js

--- src/config/command_toggles.js
+++ src/config/command_toggles.js
@@ -4,6 +4,7 @@ export const CommandToggles = {
     // Keep these active (from your birthday list)
     birthday_set: true,
     birthday_list: true,
+    announcementslot: true, // <-- ADD THIS LINE
     
     // --- USELESS COMMANDS TO HIDE FROM EVERYONE ---
     // Change any of these to true if you ever want them back!
